28360fdf95a630740c9f8fb74c2ef592c712602d
Tiger <m7mdo_o@yahoo.com>
2023-05-28 16:37:11 +0300

    feat: add jest & react testing library

---
+++ src/components/test.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('ScoreBoard', () => {
+  it('Show msg when there are no games', () => {
+    expect(true).toBe(true);
+  });
+});
